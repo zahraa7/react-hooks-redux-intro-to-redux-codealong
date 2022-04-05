a3fad63a987baecad9117dbd4ef54ae2153d9ae9
zahraa7 <zahraa.knass@hotmail.com>
2022-04-05 12:15:54 +0300

    done

--- src/index.js
+++ src/index.js
@@ -3,5 +3,13 @@ import ReactDOM from "react-dom";
 import counterReducer from "./features/counter/counterSlice.js";
 import App from "./App";
 import "./index.css";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(counterReducer);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root"),
+);
